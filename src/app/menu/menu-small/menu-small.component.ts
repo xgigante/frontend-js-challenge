0a1911cac4607ba309586eeb3f3dffbf0c0a2e37
JAvi <xgigante@gmail.com>
2024-10-24 22:08:41 +0200

    Extract HTML from menu components

--- src/app/menu/menu-small/menu-small.component.ts
+++ src/app/menu/menu-small/menu-small.component.ts
@@ -2,34 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-small',
-  template: `
-    <a
-      [routerLink]="['/trends', { provider: 'elpais' }]"
-      routerLinkActive="router-link-active"
-      [routerLinkActiveOptions]="{
-        matrixParams: 'exact',
-        queryParams: 'exact',
-        paths: 'exact',
-        fragment: 'exact'
-      }"
-      class="menu__option"
-    >
-      <img src="assets/Iconos/Favicon/favicon_el_pais.svg" alt="Icono de EL PAÍS" />
-    </a>
-    <a
-      [routerLink]="['/trends', { provider: 'elmundo' }]"
-      routerLinkActive="router-link-active"
-      [routerLinkActiveOptions]="{
-        matrixParams: 'exact',
-        queryParams: 'exact',
-        paths: 'exact',
-        fragment: 'exact'
-      }"
-      class="menu__option"
-    >
-      <img src="assets/Iconos/Favicon/favicon_el_mundo.svg" alt="Icono de EL MUNDO" />
-    </a>
-  `,
+  templateUrl: './menu-small.component.html',
   styleUrls: ['./menu-small.component.scss'],
 })
 export class MenuSmallComponent {}
