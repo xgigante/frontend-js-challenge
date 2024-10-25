dfe4e91a69a8b7ca5fc02fcbb812bdd59fcc6f98
JAvi <xgigante@gmail.com>
2024-10-25 10:21:25 +0200

    Extract HTML from trends components

--- src/app/trends/trends-list/trends-list.component.ts
+++ src/app/trends/trends-list/trends-list.component.ts
@@ -6,19 +6,7 @@ import { selectTrendsByProvider } from '../store/selectors';
 
 @Component({
   selector: 'app-trends-list',
-  template: `
-    <article class="trend" *ngFor="let trend of trends$ | async">
-      <a class="trend__link" routerLink="/trends/{{ trend.id }}">
-        <figure class="trend__figure">
-          <img class="trend__image" [src]="trend.image" [alt]="trend.title" />
-          <figcaption class="trend__title">
-            <h2>{{ trend.title }}</h2>
-          </figcaption>
-        </figure>
-        <p class="trend__excerpt">{{ trend.body[0] }}</p>
-      </a>
-    </article>
-  `,
+  templateUrl: './trends-list.component.html',
   styleUrls: ['./trends-list.component.scss'],
 })
 export class TrendsListComponent implements OnInit {
